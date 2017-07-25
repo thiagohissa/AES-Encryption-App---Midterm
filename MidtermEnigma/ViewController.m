9763bf661d0787d6b3a9d1a31755d36c273bf834
Thiago Hissa <thiagohissa@gmail.com>
2017-07-25 09:58:00 -0400

    added motion background2

--- MidtermEnigma/ViewController.m
+++ MidtermEnigma/ViewController.m
@@ -20,6 +20,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"WebViewContent2" ofType:@"html"];
+    NSURL *htmlURL = [[NSURL alloc] initFileURLWithPath:htmlPath];
+    NSData *htmlData = [[NSData alloc] initWithContentsOfURL:htmlURL];
+    
+    [self.myWebView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:[htmlURL URLByDeletingLastPathComponent]];
+    
+    
+    CALayer *bottomBorder1 = [CALayer layer];
+    bottomBorder1.frame = CGRectMake(0.0f, self.mainTextField.frame.size.height - 1, self.mainTextField.frame.size.width, 0.3f);
+    bottomBorder1.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.mainTextField.layer addSublayer:bottomBorder1];
+    
+    CALayer *bottomBorder2 = [CALayer layer];
+    bottomBorder2.frame = CGRectMake(0.0f, self.keyTextField.frame.size.height - 1, self.keyTextField.frame.size.width, 0.3f);
+    bottomBorder2.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.keyTextField.layer addSublayer:bottomBorder2];
+    
+    UIColor *color = [[UIColor whiteColor] colorWithAlphaComponent:0.3];
+    self.mainTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Insert Text Here" attributes:@{NSForegroundColorAttributeName: color}];
+    self.keyTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Key" attributes:@{NSForegroundColorAttributeName: color}];
 }
 
 
